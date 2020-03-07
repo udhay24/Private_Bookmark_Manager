72e2db25fdb90b47d9dffd74ea09c7a75e91d518
udhay24 <udhay2498@gmail.com>
2020-03-07 11:35:51 +0530

    Initial commit

---
+++ animals.js
@@ -0,0 +1,8 @@
+console.clear();
+console.log("content script has been started");
+
+images = document.images;
+for (let image of images) {
+    // image.src = "https://source.unsplash.com/random"
+}
+
